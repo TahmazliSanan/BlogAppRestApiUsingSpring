8885d8d21b058f73636d07c17121a514e12aa0c3
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-12-27 21:51:52 +0400

    Create "CategoryDto"

---
+++ src/main/java/com/project/blogapprestapi/payloads/CategoryDto.java
@@ -0,0 +1,16 @@
+package com.project.blogapprestapi.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryDto {
+    private Integer id;
+    private String title;
+    private String description;
+}
