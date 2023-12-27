79ef2cb84204ca62c705634fc3b9744175347d8c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-12-27 21:10:22 +0400

    Add "@Valid" annotation

--- src/main/java/com/project/blogapprestapi/controllers/UserController.java
+++ src/main/java/com/project/blogapprestapi/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.project.blogapprestapi.controllers;
 import com.project.blogapprestapi.payloads.ApiResponse;
 import com.project.blogapprestapi.payloads.UserDto;
 import com.project.blogapprestapi.services.inters.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +18,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         return new ResponseEntity<>(userService.createUser(userDto), HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(
-            @RequestBody UserDto userDto,
-            @PathVariable("userId") Integer userId) {
-        return new ResponseEntity<>(userService.updateUser(userDto, userId), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
@@ -36,6 +30,13 @@ public class UserController {
     @GetMapping("/")
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(
+            @Valid @RequestBody UserDto userDto,
+            @PathVariable("userId") Integer userId) {
+        return new ResponseEntity<>(userService.updateUser(userDto, userId), HttpStatus.OK);
     }
 
     @DeleteMapping("/{userId}")
