ad2264eea3fb8f525d3b5dcaed67d87c3ca5946a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-12-27 21:03:42 +0400

    Add "MethodArgumentNotValidException" handler method

--- src/main/java/com/project/blogapprestapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/project/blogapprestapi/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.project.blogapprestapi.exceptions;
 import com.project.blogapprestapi.payloads.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +17,11 @@ public class GlobalExceptionHandler {
         String message = exception.getMessage();
         ApiResponse apiResponse = new ApiResponse(message, false);
         return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgsNotValidExceptionHandler(MethodArgumentNotValidException exception) {
+        Map<String, String> response = new HashMap<>();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
