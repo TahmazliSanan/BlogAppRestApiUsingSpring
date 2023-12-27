b408d14abbe9f35ea8a1390d7c372f897d2da86a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-12-27 13:22:54 +0400

    Create project in Spring

---
+++ src/test/java/com/project/blogapprestapi/BlogAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.blogapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
