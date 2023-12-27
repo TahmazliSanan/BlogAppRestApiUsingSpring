7aabc3c806a030c663d7180c6748c904722a704d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-12-27 20:58:34 +0400

    Add validate annotations

--- src/main/java/com/project/blogapprestapi/payloads/UserDto.java
+++ src/main/java/com/project/blogapprestapi/payloads/UserDto.java
@@ -1,5 +1,8 @@
 package com.project.blogapprestapi.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,12 @@ import lombok.Setter;
 @Setter
 public class UserDto {
     private Integer id;
+    @NotEmpty(message = "Full name cannot be empty!")
     private String fullName;
+    @Email(message = "Email address is not valid!")
     private String emailAddress;
+    @NotEmpty(message = "Password cannot be empty!")
+    @Size(min = 8, max = 16, message = "Password's length must be between 8 and 16!")
     private String password;
     private String aboutBio;
 }
