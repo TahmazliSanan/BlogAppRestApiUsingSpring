31474cfc22f33172a4136136ac9866b5b9a0591b
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-12-28 19:48:12 +0400

    Change generic type

--- src/main/java/com/project/blogapprestapi/repositories/PostRepository.java
+++ src/main/java/com/project/blogapprestapi/repositories/PostRepository.java
@@ -1,4 +1,13 @@
 package com.project.blogapprestapi.repositories;
 
-public interface PostRepository {
+import com.project.blogapprestapi.entities.Category;
+import com.project.blogapprestapi.entities.Post;
+import com.project.blogapprestapi.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByUser(User user);
+    List<Post> findByCategory(Category category);
 }
