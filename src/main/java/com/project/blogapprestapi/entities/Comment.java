8220000f769c60593e015545e38a57b326e22be7
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-01-01 15:37:36 +0400

    Create "Comment" entity

---
+++ src/main/java/com/project/blogapprestapi/entities/Comment.java
@@ -0,0 +1,23 @@
+package com.project.blogapprestapi.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String content;
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
